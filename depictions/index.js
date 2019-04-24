17d4c4e97135822c24fd6e8e40cc2094ed4d1080
sPEeDMx <aek_speedmx@hotmail.com>
2019-04-24 10:13:32 +0700

    Update index.js

--- depictions/index.js
+++ depictions/index.js
@@ -168,7 +168,7 @@ $(document).ready(function () {
         var extra = {
             //"<img class=\"icon\" src=\"icons/twitter.png\"><span>Find me on Twitter (@sPEeDMx)</span>": "https://twitter.com/speedmx",
             "<img class=\"icon\" src=\"icons/email.png\"><span>ส่งอีเมล์</span>": "mailto:aek.speedmx@gmail.com",
-            "<img class=\"icon\" src=\"icons/paypal.png\"><span>Buy me a &#x2615; via <span style=\"font-style:italic;font-weight:bold;\"><span style=\"color:#253b80;\">Pay</span><span style=\"color:#419bd7;\">Pal</strong></span></span>": "https://paypal.me/speedmx"
+            "<img class=\"icon\" src=\"icons/like.png\"><span>สนับสนุน &#x2615; <span style=\"font-style:italic;font-weight:bold;\"><span style=\"color:#253b80;\">Pay</span><span style=\"color:#419bd7;\">Pal</strong></span></span>": "https://paypal.me/speedmx"
         };
         $.extend(links, extra);
         var lKeys = Object.keys(links);
